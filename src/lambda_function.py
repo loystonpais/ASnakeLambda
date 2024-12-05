b588b75e8c70faec73197ec7af33fa76e746dae9
loystonpais <loyston500@gmail.com>
2024-12-05 23:46:24 +0530

    refactor: Improved lambda function robustness with enhanced code execution and error handling.

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,35 +4,52 @@ import os
 from ASnake import build
 
 def lambda_handler(event, context):
-    # Check if "code" key is in the event
-    if "code" not in event:
-        return {
-            'statusCode': 400,
-            'body': json.dumps("Error: 'code' key not found in event.")
-        }
 
-    # Get code from event
-    source = event["code"]
-
-    # Write the generated code to a temporary file
-    temp_file = "/tmp/generated_code.py"
-    with open(temp_file, "w") as file:
-        file.write(source)
-
-    # Execute the generated Python code using a subprocess
     try:
-        result = subprocess.run(
-            ["python3", temp_file],
-            text=True,
-            capture_output=True,
-            check=True
-        )
-        output = result.stdout.strip()
-    except subprocess.CalledProcessError as e:
-        output = f"Error: {e.stderr.strip()}"
 
-    # Return the output or error
-    return {
-        'statusCode': 200,
-        'body': json.dumps(output)
-    }
+        body = event["body"]
+
+        # Check if "code" key is in the event body
+        if "code" not in body:
+            return {
+                'statusCode': 400,
+                'body': json.dumps("Error: 'code' key not found in event body.")
+            }
+        
+        # Convert body to json
+        body = json.loads(body)
+
+        # Get code from event body
+        asnake_source = body["code"]
+
+        # Build the source code using the ASnake library
+        source = build(asnake_source)
+
+        # Write the generated code to a temporary file
+        temp_file = "/tmp/transpiled.py"
+        with open(temp_file, "w") as file:
+            file.write(source)
+
+        # Execute the generated Python code using a subprocess
+        try:
+            result = subprocess.run(
+                ["python3", temp_file],
+                text=True,
+                capture_output=True,
+                check=True
+            )
+            output = result.stdout.strip()
+        except subprocess.CalledProcessError as e:
+            output = f"Error: {e.stderr.strip()}"
+
+        # Return the output or error
+        return {
+            'statusCode': 200,
+            'body': json.dumps(output)
+        }
+    except Exception as e:
+        # Server exceptions
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f"Server Error: {str(e)}")
+        }
